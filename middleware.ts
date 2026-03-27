964bbaf3f670080b15a4eebfe9e6a7d9b74db7d0
edwinzhangindo-svg <edwinzhang.indo@gmail.com>
2026-03-27 14:42:20 +0800

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,25 +1,5 @@
-import { withAuth } from 'next-auth/middleware'
-import { NextResponse } from 'next/server'
-
-export default withAuth(
-  function middleware(req) {
-    return NextResponse.next()
-  },
-  {
-    callbacks: {
-      authorized({ token, req }) {
-        const { pathname } = req.nextUrl
-        if (pathname.startsWith('/admin/login')) return true
-        if (pathname.startsWith('/admin')) return !!token
-        return true
-      },
-    },
-    pages: {
-      signIn: '/admin/login',
-    },
-  }
-)
+export { default } from 'next-auth/middleware'
 
 export const config = {
-  matcher: ['/admin/:path*'],
+  matcher: ['/admin/dashboard/:path*', '/admin/services-mgmt/:path*'],
 }
